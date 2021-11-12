6899c81cc830123cf8ff83b1976e70756b7bfa2b
Aidan Murray <aidanm0121@gmail.com>
2021-11-11 20:03:51 -0700

    Update secrets_template.py

--- main/secrets_template.py
+++ main/secrets_template.py
@@ -1,5 +1,5 @@
 # Step 1: Rename this file secrets.py
-# Step 2: Fill out the following info (most found on the spotify dev dashboard)
+# Step 2: Fill out the following info (most found on the spotify dev dashboard )
 
 CLIENT_ID = ""  # Your Spotify App's Client ID
 CLIENT_SECRET = ""  # Your Spotify App's Client Secret
